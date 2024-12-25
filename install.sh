2a95e4536b945f53bc28e8832d4e9b9d119df30a
weil <alexwei0609@gmail.com>
2024-12-25 13:44:46 +0800

    corrected wrong instruction to install PyTorch

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121
+# This is problematic, missing pytorch-cuda
+# pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121
+# Correction
+# conda install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 
 pip install -e .
 
